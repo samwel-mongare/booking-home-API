bcbda82ef2d4177dd01e547909b10b2fcce0f6ba
youmari <youmari300@gmail.com>
2022-04-28 14:50:09 +0000

    Create routes for sessions registrations, logout and logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   # Defines the root path route ("/")
   # root "articles#index"
 end
