6512757bc023f51b2d4ab4c94463d9393d04d94a
youmari <youmari300@gmail.com>
2022-04-28 16:05:59 +0000

    Set up session_so  Rails.application.config.session_store :cookie_store, key: '_authentication', domain: ''

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication', domain: ''
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: '_authentication', domain: ''
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication'
+end
