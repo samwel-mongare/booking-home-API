b6ca30dcbd482d8937c8cba6727ff7059b041a2b
youmari <youmari300@gmail.com>
2022-04-28 14:53:08 +0000

    Create actions to allow user to register

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < ApplicationController
+  def create
+    @user = User.new(name: params['user']['name'], email: params['user']['email'],
+                     password: params['user']['password'], password_confirmation: params['user']['password_confirmation'])
+    if @user.save
+      session[:user_id] = @user.id
+      render json: {
+        status: :created, user: @user
+      }
+    else
+      render json: { status: 400, errors: @user.errors.full_messages }
+    end
+  end
+end
