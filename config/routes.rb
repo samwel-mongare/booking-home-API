1f7081baaadccc38470e211c5dfd983cfb31db60
mong'are <mongaresams@gmail.com>
2022-04-30 03:03:04 +0300

    Update api routes and added versioning

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
+
+  namespace :api do
+    namespace :v1 do  
+      resources :apartments, only: %i[index create update destroy]
+      resources :rentals, only: %i[index create update destroy]
+      resources :markets, only: %i[index create update destroy]
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
