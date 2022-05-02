7aca93052f530fb68be4da55f5975ca2c4cf9b6d
mong'are <mongaresams@gmail.com>
2022-05-02 18:11:50 +0300

    Implement remove apartment endpoint

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,6 @@
 class Apartment < ApplicationRecord
   belongs_to :owner, class_name: 'User'
+  has_many :rentals, dependent: :destroy
+  has_many :markets, dependent: :destroy
+  has_many :user_apartments, dependent: :destroy
 end
