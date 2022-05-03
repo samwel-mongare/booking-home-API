c37f2c112d07fcbaf88ccc12b72a038836320e43
mong'are <mongaresams@gmail.com>
2022-05-03 11:23:58 +0300

    Add view user's favourite apartments

--- app/controllers/api/v1/user_apartments_controller.rb
+++ app/controllers/api/v1/user_apartments_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UserApartmentsController < ApplicationController
   def index
-    @user_apartments = Apartment.where(id: params[:apartment_id])
+    @user_favourite = UserApartment.where(user_id: current_user.id)
+    @user_apartments = Apartment.where(id: @user_favourite.map(&:apartment_id))
     render json: ApartmentsRepresenter.new(@user_apartments).as_json
   end
 end
