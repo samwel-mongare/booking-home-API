d5e3033b39bf5d4227b3f48578b68a90b1dc0c17
Meri <mgogi13@freeuni.edu.ge>
2022-05-05 13:33:52 +0400

    feat: api documentation for sessions and registration

---
+++ spec/requests/registrations_spec.rb
@@ -0,0 +1,39 @@
+require 'swagger_helper'
+
+RSpec.describe 'registrations', type: :request do
+
+  path '/registrations' do
+
+    post('create registration') do
+      tags 'sign up'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :registration, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: { type: :object,
+          properties: {
+            name: { type: :string },
+            email: { type: :string },
+            password: { type: :string},
+            password_confirmation: { type: :string }
+          }
+        }
+      },
+        required: %w[name email password password_confirmation]
+      }
+
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,71 @@
+require 'swagger_helper'
+
+RSpec.describe 'sessions', type: :request do
+
+  path '/sessions' do
+
+    post('create session') do
+      tags 'sessions'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: { 
+            type: :object,
+            properties: {
+              email: { type: :string },
+              password: { type: :string }
+            }
+          }
+        },
+        required: %w[email password]
+      }
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/logout' do
+    delete('logout session') do
+      tags 'sessions'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/logged_in' do
+    get('logged_in session') do
+      tags 'sessions'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
