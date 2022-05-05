803143c2186ccb421d7af9e9c4783b0aff49a8ca
Meri <mgogi13@freeuni.edu.ge>
2022-05-05 17:52:01 +0400

    feat: add address for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,9 +5,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     credentials: true
   end
   # this is for production 
-  # allow do
-  #   origins "production domain"
-  #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete],
-  #   credentials: true
-  # end
+  allow do
+    origins "http://localhost:3001"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete],
+    credentials: true
+  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_authentication', domain: ''
+  Rails.application.config.session_store :cookie_store, key: '_authentication', domain: 'http://localhost:3001'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication'
 end
